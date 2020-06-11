8064df92eb93d435808749b7216e3c0ef3b16008
Erik Slovak <erik.slovak@technologystudio.sk>
2020-06-11 13:58:35 +0200

    add exports

---
+++ packages/@txo/startup-react/src/index.js
@@ -0,0 +1,24 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2020-06-11T11:06:10+02:00
+ * @Copyright: Technology Studio
+ * @flow
+**/
+
+import { redux } from './Redux'
+import * as StartupRedux from './Redux/Model/StartupRedux'
+import {
+  selectStartupComplete,
+  selectIsRehydrated,
+} from './Redux/Model/StartupSelectors'
+import { rootSaga } from './Sagas'
+import { waitForStartupComplete } from './Sagas/StartupSagas'
+
+export {
+  redux,
+  rootSaga,
+  selectIsRehydrated,
+  selectStartupComplete,
+  StartupRedux,
+  waitForStartupComplete,
+}
