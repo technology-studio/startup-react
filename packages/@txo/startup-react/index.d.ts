8f4707c5df5ac14c042b44d3a173921c927424b2
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2021-02-05 19:45:10 +0100

    Add packages shortcut

--- packages/@txo/startup-react/index.d.ts
+++ packages/@txo/startup-react/index.d.ts
@@ -1,2 +1,9 @@
 declare module "@txo/startup-react" {
+  import type { SagaIterator, Effect } from 'redux-saga'
+  type SagaGenerator<RT = void> = SagaIterator<RT> & {
+    [Symbol.iterator](): SagaGenerator<RT>;
+  }
+
+  function waitForStartupComplete (effect: Effect): SagaGenerator<void>
 }
+
