dc748f75e228fb9b65c4dc59bf0938fac694874b
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2022-10-25 08:26:53 +0200

    fix: add augmentation of DefaultRootState (#47)
    * chore: install redux peer package and remove old types
    
    * fix: augment DefaultRootState
    
    * chore: update txo redux
    
    * chore: add @txo-peer-dep/redux to peer deps
    
    * chore: resolve dependencies in yarn.lock
    
    * chore: remove redundant dev dependency
    
    Co-authored-by: Rostislav Simonik <rostislav.simonik@technologystudio.sk>

--- src/Redux/Model/StartupSelectors.ts
+++ src/Redux/Model/StartupSelectors.ts
@@ -4,7 +4,7 @@
  * @Copyright: Technology Studio
 **/
 
-import type { DefaultRootState } from 'react-redux'
+import type { DefaultRootState } from '@txo-peer-dep/redux'
 
 export const selectStartupComplete = (state: DefaultRootState): boolean | null => state.startup.model.startup.startupComplete
 export const selectIsRehydrated = (state: DefaultRootState): boolean => !!state.startup.model.startup.rehydrated
